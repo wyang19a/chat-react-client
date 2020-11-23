39c745046a3780b06996806e40c51bfc449dcbfc
wyang19a <wyang19a@gmail.com>
2020-11-23 22:18:21 +0900

    Adds Post message functionality

--- src/components/Chat/ChatInputField.js
+++ src/components/Chat/ChatInputField.js
@@ -1,21 +1,37 @@
 import React, { Fragment, useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
 import { createMessage } from '../../api/message'
 
 export default function ChatInputField ({ chatSessionId, user }) {
   const [message, setMessage] = useState('')
 
-  const handleMessage = (e) => {
+  const handleChange = (e) => {
     setMessage(e.target.value)
   }
-  const handlethis = () => {
+
+  const onMessageSubmit = (e) => {
+    e.preventDefault()
     createMessage(message, chatSessionId, user)
-      .then(console.log('success!'))
+      .then(setMessage(''))
   }
+
   return (
     <Fragment>
-      <textarea onChange={handleMessage} placeholder="Your message here" />
-      <button onClick={handlethis}>Submit</button>
-      {/* TODO: Change it to form, and then submit */}
+      <Form onSubmit={onMessageSubmit}>
+        <Form.Group controlId="message">
+          <Form.Control
+            required
+            name="message"
+            value={message}
+            type="textarea"
+            placeholder="Your message here"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </Fragment>
   )
 }
