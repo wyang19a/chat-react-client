0f1ea2334bf8745a4868e8572e9bf291799c3988
wyang19a <wyang19a@gmail.com>
2020-11-23 22:17:13 +0900

    Styles the messages

--- src/components/Chat/ChatMessages.js
+++ src/components/Chat/ChatMessages.js
@@ -1,11 +1,49 @@
 import React, { Fragment } from 'react'
+import styled from 'styled-components'
+
+const MessageStyle = styled.div`
+  margin-top: 0.5rem;
+  .message-text {
+    border: 1px solid black;
+    border-radius: 10px;
+    padding: 5px;
+  }
+  box-sizing: border-box;
+`
+
+const UserMessageStyle = styled.div`
+  text-align: right;
+  display: flex;
+  justify-content: flex-end;
+  gap: 20px;
+  .message-text {
+  background-color: lightblue;
+  }
+`
+const OthersMessagesStyle = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  gap: 20px;
+  .message-text {
+  background-color: lightgrey;
+  }
+`
 
 export default function ChatMessages ({ messages, user }) {
-  console.log(messages)
+  // console.log(messages)
   return (
     <Fragment>
       {messages && messages.map(message => (
-        <p key={message._id}>{message.text}</p>
+        <MessageStyle key={message._id}>
+          {user._id === message.owner
+            ? <UserMessageStyle>
+              <span className='message-text'>{message.text}</span><span className='message-user-name'>{message.nickName}</span>
+            </UserMessageStyle>
+            : <OthersMessagesStyle>
+              <span className='message-user-name'>{message.nickName}</span><span className='message-text'>{message.text}</span>
+            </OthersMessagesStyle>
+          }
+        </MessageStyle>
       ))}
     </Fragment>
   )
